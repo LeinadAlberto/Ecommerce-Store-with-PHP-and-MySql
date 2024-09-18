b695ac370501fe2ff3bd25f0fb391dabd659b466
Alberto <dany.canaviri2022@gmail.com>
2024-09-17 23:35:39 -0400

    Diseño del Footer de la plantilla finalizado

--- web/views/modules/footer.php
+++ web/views/modules/footer.php
@@ -1,8 +1,262 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-        Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+<style>
+    a:link, a:visited, a:hover, a:active {
+        text-decoration: none !important;
+        color: inherit !important;
+    }
+</style>
+
+<div class="container-fluid bg-dark small">
+
+    <div class="container py-5 text-light">
+
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+
+            <div class="col row">
+
+                <!-- Ropa -->
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Ropa</a>
+
+                    </h4>
+
+                    <hr class="border-white">
+
+                    <ul>
+
+                        <li>
+                            <a href="#">Ropa para dama</a>
+                        </li>
+                        <li>
+                            <a href="#">Ropa para hombre</a>
+                        </li>
+                        <li>
+                            <a href="#">Ropa deportiva</a>
+                        </li>
+                        <li>
+                            <a href="#">Ropa infantil</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+                
+                <!-- Calzado -->
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Calzado</a>
+
+                    </h4>
+
+                    <hr class="border-white">
+
+                    <ul>
+
+                        <li>
+                            <a href="#">Calzado para dama</a>
+                        </li>
+                        <li>
+                            <a href="#">Calzado para hombre</a>
+                        </li>
+                        <li>
+                            <a href="#">Calzado deportivo</a>
+                        </li>
+                        <li>
+                            <a href="#">Calzado infantil</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <!-- Tecnología -->
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Tecnología</a>
+
+                    </h4>
+
+                    <hr class="border-white">
+
+                    <ul>
+
+                        <li>
+                            <a href="#">Teléfonos Móvil</a>
+                        </li>
+                        <li>
+                            <a href="#">Tabletas</a>
+                        </li>
+                        <li>
+                            <a href="#">Electrónicos</a>
+                        </li>
+                        <li>
+                            <a href="#">Computadoras</a>
+                        </li>
+                        <li>
+                            <a href="#">Auriculares</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <!-- Cursos -->
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Cursos</a>
+
+                    </h4>
+
+                    <hr class="border-white">
+
+                    <ul>
+
+                        <li>
+                            <a href="#">Desarrollo Web</a>
+                        </li>
+                        <li>
+                            <a href="#">Aplicaciones</a>
+                        </li>
+                        <li>
+                            <a href="#">Móviles</a>
+                        </li>
+                        <li>
+                            <a href="#">Diseño Gráfico</a>
+                        </li>
+                        <li>
+                            <a href="#">Marketing Digital</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </div><!-- /.col -->
+
+            <div class="col my-3 my-lg-0 px-lg-5 text-light">
+
+                <h1 class="lead small">Dudas e inquietudes, contáctenos en:</h1>
+
+                <br>
+
+                <h1 class="lead small">
+
+                    <i class="fa fa-phone-square pe-2"></i> (591) 69945532
+
+                    <br><br>
+
+                    <i class="fa fa-envelope pe-2"></i> lokywebdev@gmail.com
+
+                    <br><br>
+
+                    <i class="fa fa-map-marker pe-2"></i> Calle 3, Miraflores, Av. B
+
+                    <br><br>
+
+                    La Paz | Bolivia
+
+                </h1>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19164.648876142222!2d-68.12560593350472!3d-16.510631136613302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915f20884a7c16d7%3A0x36c3a3ee4d4dc9d!2sPlaza%20Espa%C3%B1a!5e1!3m2!1ses-419!2sbo!4v1726630232736!5m2!1ses-419!2sbo" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+            </div><!-- /.col -->
+            
+            <div class="col small my-3 my-lg-0">
+
+                <h4>RESUELVA SU INQUIETUD</h4>
+
+                <form role="form" method="post">
+
+                    <input type="text" id="nombreContactenos" name="nombreContactenos" class="form-control" placeholder="Escriba su nombre" required>
+
+                    <br>
+
+                    <input type="email" id="emailContactenos" name="emailContactenos" class="form-control" placeholder="Escriba su correo electrónico" required>
+
+                    <br>
+
+                    <textarea id="mensajeContactenos" name="mensajeContactenos" class="form-control" placeholder="Escriba su mensaje" rows="5" required></textarea>
+
+                    <br>
+
+                    <input type="submit" value="Enviar" class="btn btn-default templateColor float-right border-0" style="background: #0CC0DF; color: white;">
+
+                </form>
+
+            </div><!-- /.col -->
+
+        </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+</div><!-- /.container-fluid -->
+
+<footer class="main-footer" style="background: black; color: white;">
+
+    <div class="container">
+        
+        <div class="float-end">
+
+            <div class="d-flex justify-content-center" style="line-height: 0px;">
+
+                <div class="p-2">
+
+                    <a href="https://facebook.com" target="_blank" class="text-white">
+
+                        <i class="fab fa-facebook-f"></i>
+
+                    </a>
+
+                </div>
+
+                <div class="p-2">
+
+                    <a href="https://youtube.com" target="_blank" class="text-white">
+
+                        <i class="fab fa-youtube"></i>
+
+                    </a>
+
+                </div>
+
+                <div class="p-2">
+
+                    <a href="https://twitter.com" target="_blank" class="text-white">
+
+                        <i class="fab fa-twitter"></i>
+
+                    </a>
+
+                </div>
+
+                <div class="p-2">
+
+                    <a href="https://instagram.com" target="_blank" class="text-white">
+
+                        <i class="fab fa-instagram"></i>
+
+                    </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <small>
+            &copy; <?php echo date("Y"); ?> 
+            Todos los derechos reservados. Sitio elaborado por 
+            <a href="lokywebdev.com" style="color: #0CC0DF !important;">LokyWebDev</a>.
+        </small>
+
+    </div><!-- /.container -->
+
 </footer>
