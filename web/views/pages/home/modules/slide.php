a2d038cfce9b0e14975ec97ee6ff6b3cb27ef091
Alberto <dany.canaviri2022@gmail.com>
2024-09-19 23:30:11 -0400

    Se creo la estructura de la página de inicio Home

---
+++ web/views/pages/home/modules/slide.php
@@ -0,0 +1,53 @@
+<div class="jd-slider">
+
+    <div class="slide-inner">
+
+        <ul class="slide-area">
+
+            <li>
+
+                <img src="<?php echo $path ?>views/assets/img/slide/1/back_default.jpg" class="img-fluid">
+
+            </li>
+
+            <li>
+
+                <img src="<?php echo $path ?>views/assets/img/slide/2/fondo2.jpg" class="img-fluid">
+
+            </li>
+
+            <li>
+
+                <img src="<?php echo $path ?>views/assets/img/slide/3/fondo3.jpg" class="img-fluid">
+
+            </li>
+
+        </ul><!-- /.slide-area -->
+    
+    </div><!-- /.slide-inner -->
+
+    <a class="prev" href="#">
+
+        <i class="fas fa-angle-left fa-2x"></i>
+
+    </a>
+
+    <a class="next" href="#">
+
+        <i class="fas fa-angle-right fa-2x"></i>
+
+    </a>
+
+    <div class="controller">
+
+        <div class="indicate-area"></div>
+
+    </div>
+
+</div><!-- /.jd-slider -->
+
+<script>
+
+    $('.jd-slider').jdSlider();
+
+</script>
--- web/views/template.php
+++ web/views/template.php
@@ -24,6 +24,8 @@ $path = TemplateController::path();
         <link rel="stylesheet" href="<?php echo $path ?>views/assets/css/plugins/fontawesome-free/css/all.min.css">
         <!-- Latest compiled and minified CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- JDSlider -->
+        <link rel="stylesheet" href="<?php echo $path ?>views/assets/css/plugins/jdSlider/jdSlider.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="<?php echo $path ?>views/assets/css/plugins/adminlte/adminlte.min.css">
         <!-- Custom CSS -->
@@ -36,6 +38,10 @@ $path = TemplateController::path();
         <script src="<?php echo $path ?>views/assets/js/plugins/jquery/jquery.min.js"></script>
         <!-- Latest compiled JavaScript -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+        <!-- JDSlider -->
+         <!-- https://www.jqueryscript.net/slider/Carousel-Slideshow-jdSlider.html -->
+        <script src="<?php echo $path ?>views/assets/js/plugins/jdSlider/jdSlider.js"></script>
 
     </head>
 
@@ -55,98 +61,15 @@ $path = TemplateController::path();
                 include "modules/sidebar.php";
                 /* /.main sidebar container */
 
+                /* Content Wrapper. Contains page content */
+                include "pages/home/home.php";
+                /* /.content-wrapper */
+            
+                /* Main Footer */
+                include "modules/footer.php"; 
+                /* /.main footer */
+
             ?>
-           
-            <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <div class="content-header">
-                <div class="container">
-                    <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0"> Top Navigation <small>Example 3.0</small></h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Layout</a></li>
-                        <li class="breadcrumb-item active">Top Navigation</li>
-                        </ol>
-                    </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-                </div>
-                <!-- /.content-header -->
-
-                <!-- Main content -->
-                <div class="content">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-
-                            <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's
-                            content.
-                            </p>
-
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-
-                            <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's
-                            content.
-                            </p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                        </div><!-- /.card -->
-                    </div>
-                    <!-- /.col-md-6 -->
-                    <div class="col-lg-6">
-                        <div class="card">
-                        <div class="card-header">
-                            <h5 class="card-title m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5 class="card-title m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        </div>
-                    </div>
-                    <!-- /.col-md-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div><!-- /.container-fluid -->
-                </div>
-                <!-- /.content -->
-            </div><!-- /.content-wrapper -->
-
-            <!-- Main Footer -->
-            <?php include "modules/footer.php"; ?>
-            <!-- /.main footer -->
 
         </div><!-- ./wrapper -->
 
